8994aafd1dc5a1c2e55b76a771653e64dbfe99b4
Raktim Mazumdar <raktimmazumdar11@gmail.com>
2024-02-03 23:50:27 +0530

    First Commit

---
+++ Radar_Chart.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  8 21:08:47 2023
+
+@author: Raktim Mazumdar
+"""
+
+# Importing the Libraries
+
+pip install plotly
+import pandas as pd #handle the dataframes
+import numpy as np #Importing Numpy Library
+import matplotlib.pyplot as plt #Importing MatPlotLib Library
+import plotly.express as px #plot the spider charts
+
